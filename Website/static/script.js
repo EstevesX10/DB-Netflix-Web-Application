1a48d889809e134a30563cf415468874b0742a34
Gongas <goncalo.mesteves03@gmail.com>
2023-12-21 19:28:32 +0000

    Pagination Update using ChatGPT's help

--- Website/static/script.js
+++ Website/static/script.js
@@ -1,64 +1,75 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+/* 
+# ---------- #
+| Pagination |
+# ---------- #
+*/
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-});
-
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
-});
-
-/* ------------------------------------------------ */
-
-// Pagination 
-
-let link = document.getElementsByClassName("link");
-
-let currentValue = 1;
-
-function activeLink () {
-    for (l of link){
-        l.classList.remove("active");
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Function to determine the initial page based on the URL
+    function determineInitialPage() {
+        let currentPath = window.location.pathname;
+        let match = currentPath.match(/\/movies\/P(\d+)/);
+        return match ? parseInt(match[1], 10) : 1;
     }
-    event.target.classList.add("active");
-    currentValue = event.target.value;
-}
 
-function backBtn() {
-    if (currentValue > 1){
-        for (l of link){
-            l.classList.remove("active");
+    // Global variables
+    let currentValue = determineInitialPage();
+    let links = document.getElementsByClassName('link');
+
+    // Function to update active link
+    function updateActiveLink() {
+        Array.from(links).forEach(link => link.classList.remove('active'));
+        links[currentValue - 1].classList.add('active');
+    }
+
+    function activeLink(event) {
+        let page = parseInt(event.currentTarget.getAttribute('data-value'));
+
+        // Check if the page number is valid and exists
+        if (!page || page < 1 || page > links.length) {
+            console.error('Invalid page number:', page);
+            return; // Do nothing if the page number is invalid
         }
-        currentValue--;
-        link[currentValue-1].classList.add("active");
-    }
-}
 
-function nextBtn() {
-    if (currentValue < 6){
-        for (l of link){
-            l.classList.remove("active");
+        currentValue = page;
+        updateActiveLink();
+        window.location.href = '/movies/P' + page; // Redirect to the new page
+    }
+
+    // Attach event listeners to pagination links
+    Array.from(links).forEach(link => {
+        link.addEventListener('click', activeLink);
+    });
+
+    function backBtn() {
+        if (currentValue > 1) {
+            currentValue--;
+            updateActiveLink();
+            window.location.href = '/movies/P' + currentValue; // Redirect to the new page
         }
-        currentValue++;
-        link[currentValue-1].classList.add("active");
     }
-}
+    
+    function nextBtn() {
+        if (currentValue < links.length) {
+            currentValue++;
+            updateActiveLink();
+            window.location.href = '/movies/P' + currentValue; // Redirect to the new page
+        }
+    }
 
-/* ------------------------------------------------ */
+    // Attach event listeners to buttons
+    document.getElementById('btn1').addEventListener('click', backBtn);
+    document.getElementById('btn2').addEventListener('click', nextBtn);
 
-// Search 
+    // Update the active link initially
+    updateActiveLink();
+});
+
+/* 
+# ------ #
+| Search |
+# ------ #
+*/
 
 function handleKeyPress(event) {
     // Check if the pressed key is Enter (key code 13)
