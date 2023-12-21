4c06fb01fab5d7fc424779949a898a43d48b71f9
Gongas <goncalo.mesteves03@gmail.com>
2023-12-21 16:12:01 +0000

    Styling update [Pagination]

--- Website/static/script.js
+++ Website/static/script.js
@@ -22,6 +22,42 @@ iconClose.addEventListener('click', ()=> {
 
 /* ------------------------------------------------ */
 
+// Pagination 
+
+let link = document.getElementsByClassName("link");
+
+let currentValue = 1;
+
+function activeLink () {
+    for (l of link){
+        l.classList.remove("active");
+    }
+    event.target.classList.add("active");
+    currentValue = event.target.value;
+}
+
+function backBtn() {
+    if (currentValue > 1){
+        for (l of link){
+            l.classList.remove("active");
+        }
+        currentValue--;
+        link[currentValue-1].classList.add("active");
+    }
+}
+
+function nextBtn() {
+    if (currentValue < 6){
+        for (l of link){
+            l.classList.remove("active");
+        }
+        currentValue++;
+        link[currentValue-1].classList.add("active");
+    }
+}
+
+/* ------------------------------------------------ */
+
 // Search 
 
 function handleKeyPress(event) {
